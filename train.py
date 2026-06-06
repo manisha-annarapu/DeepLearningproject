519834677a8fc23552fef555979368e158858a48
manisha-annarapu <jyothiannarapu2@gmail.com>
2026-06-06 16:13:33 +0530

    Added visualization results

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 from torchvision import datasets, transforms
 from torch import nn, optim
+import matplotlib.pyplot as plt
 
 print("Deep Learning Project Started")
 
@@ -22,7 +23,7 @@ train_loader = torch.utils.data.DataLoader(
 
 model = nn.Sequential(
     nn.Flatten(),
-    nn.Linear(28*28, 128),
+    nn.Linear(28 * 28, 128),
     nn.ReLU(),
     nn.Linear(128, 10)
 )
@@ -30,17 +31,39 @@ model = nn.Sequential(
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+losses = []
+
 for epoch in range(3):
+    running_loss = 0
+
     for images, labels in train_loader:
         optimizer.zero_grad()
+
         outputs = model(images)
+
         loss = loss_fn(outputs, labels)
+
         loss.backward()
+
         optimizer.step()
 
-    print(f"Epoch {epoch+1} Completed")
+        running_loss += loss.item()
+
+    avg_loss = running_loss / len(train_loader)
+
+    losses.append(avg_loss)
+
+    print(f"Epoch {epoch+1} Loss: {avg_loss:.4f}")
 
 torch.save(model.state_dict(), "model/mnist_model.pth")
 
+plt.plot(losses)
+plt.title("Training Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.savefig("results/loss_graph.png")
+plt.show()
+
 print("Training Completed Successfully")
+print("Graph Saved in results folder")
 print("Model Saved in model folder")
